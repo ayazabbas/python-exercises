8d41b1906586d4cb70312e6b49454f6d28d63ae3
Ayaz Abbas <ayaz.abbas@qa.com>
2018-02-21 11:47:24 +0000

    Initial commit - Added first exercise with solution

---
+++ solutions/1_user_input/user_input.py
@@ -0,0 +1,10 @@
+
+print("Input a message or type 'exit' to quit.")
+userInput = input()
+
+while userInput != "exit":
+    print("You just typed " + userInput)
+    print("Type another message, or 'exit' to quit")
+    userInput = input()
+
+print("Program exiting...")
